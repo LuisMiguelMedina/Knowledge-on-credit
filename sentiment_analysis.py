f12ebac1a8dfdd8d502eae6407a27d518ed7e08e
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2024-05-26 00:08:02 -0600

    refactor: minor changes to improve readability

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -6,7 +6,7 @@ classifies the sentiment, and visualizes the counts as a bar plot.
 # LIBRARIES
 ## csv: Provides functionalities to read and write CSV files.
 import csv
-## TextBlob: A library for processing textual data, providing a simple API for common natural language processing (NLP) tasks.
+## TextBlob: A library for performing essential natural language processing (NLP) tasks. 
 from textblob import TextBlob
 ## seaborn: A library for making statistical graphics in Python.
 import seaborn as sns
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 # ----------------------------------------------------------------------- #
 # -------------------------- Utility Functions -------------------------- #
 # ----------------------------------------------------------------------- #
-def read_csv(file_path, encoding='utf-8'):
+def read_csv(file_path, encoding):
     """
     Read the content of a CSV file and return it as a list of dictionaries.
 
@@ -59,8 +59,10 @@ def classify_sentiment(polarity):
         return 'positive'
     elif polarity < 0:
         return 'negative'
-    else:
+    elif polarity == 0:
         return 'neutral'
+    else:
+        return 'unknown'
 
 def analyze_sentiments(data):
     """
@@ -72,18 +74,37 @@ def analyze_sentiments(data):
     Returns:
         list: A list of dictionaries containing the original data with additional fields for analyzed polarity and sentiment class.
     """
+    result = []
     for row in data:
-        analyzed_polarity = get_text_polarity(row['selected_text'])
+        new_row = row.copy()
+        analyzed_polarity = get_text_polarity(new_row['selected_text'])
+        new_row['analyzed_polarity'] = analyzed_polarity
         sentiment_class = classify_sentiment(analyzed_polarity)
-        row['analyzed_polarity'] = analyzed_polarity
-        row['sentiment_class'] = sentiment_class
-    return data
+        new_row['sentiment_class'] = sentiment_class
+        result.append(new_row)
+    return result
+
+def save_sentiments(OUTPUT_CSV_FILE_PATH, analyzed_data):
+    """
+    Saves analyzed sentiment data to a CSV file.
+
+    Args:
+        OUTPUT_CSV_FILE_PATH (str): The file path where the CSV file will be saved.
+        analyzed_data (list of dict): The analyzed sentiment data, where each dict represents a row.
+
+    """
+    with open(OUTPUT_CSV_FILE_PATH, 'w', newline='', encoding='ISO-8859-1') as file:
+        fieldnames = analyzed_data[0].keys()
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(analyzed_data)
 
 def visualize_sentiment_counts(path, data):
     """
     Visualize and save the sentiment class counts as a bar plot.
 
     Args:
+        path (str): The path to save the plot as a PNG file.
         data (list): A list of dictionaries containing text data and sentiments.
     """
     # Count the occurrences of each sentiment class
@@ -100,13 +121,6 @@ def visualize_sentiment_counts(path, data):
     # Save the plot as a PNG file
     plt.savefig(path)
     
-def save_sentiments(OUTPUT_CSV_FILE_PATH, analyzed_data):
-    with open(OUTPUT_CSV_FILE_PATH, 'w', newline='', encoding='ISO-8859-1') as file:
-        fieldnames = analyzed_data[0].keys()
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(analyzed_data)
-
 
 
 # ----------------------------------------------------------------------- #
@@ -123,7 +137,6 @@ def main():
     OUT_SENTIMENT_PATH = 'results/sentiments_with_analysis.csv'
     # Name of the document to store the visual
     OUT_PLOT_PATH = 'results/sentiment_counts.png'
-    
     
     # Read the CSV file content 
     tweets = read_csv(IN_TWEETS_PATH, encoding='ISO-8859-1')
